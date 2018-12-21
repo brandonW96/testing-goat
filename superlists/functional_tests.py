9eb698af8e36fc68be3c39ace4b3338dd492dfc9
Brandon <bw1126@nyu.edu>
2018-12-20 23:13:52 -0600

    added more to functional tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_start_list_and_retrive(self):
+        browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+    if __name__ == '__main__':
+        unittest.main(warnings='ignore')
